c5196916aa6a7e3e3237808d40e0177eea3e3876
Steve White <swhite@lekker.local>
2019-04-14 15:15:49 -0700

    fix yosys arguments (remove nonexistant ram4k.v file)

--- examples/blinky/synth.sh
+++ examples/blinky/synth.sh
@@ -2,5 +2,5 @@
 
 SUPRA_DIR=/opt/agm/Supra-2019.03.b0
 
-${SUPRA_DIR}/map/bin/yosys -ql blinky.log -p 'synth_agm -top Blinky -vqm Blinky.vqm'  Blinky.v prom.v ram4k.v ram-bb.v
+${SUPRA_DIR}/map/bin/yosys -ql blinky.log -p 'synth_agm -top Blinky -vqm Blinky.vqm'  Blinky.v prom.v ram-bb.v
 ${SUPRA_DIR}/bin/af -B --batch --mode NATIVE -X "set QUARTUS_SDC true" -X "set FITTING timing_more" -X "set FITTER hybrid" -X "set EFFORT highest" -X "set HOLDX default"
