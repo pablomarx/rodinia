12f9dc8d65a5a7a7eaec63e531be5a008ca0165b
Steve White <swhite@lekker.local>
2019-04-23 10:19:25 -0700

    little bit more understanding of the bar format.

--- supra/bar-dump.py
+++ supra/bar-dump.py
@@ -1,62 +1,117 @@
 #!/usr/bin/python
-
-# This has only been tested with route.AG1200.bar
-# Output is:
-# (file position, string type, int type, data length, data)
-# e.g.
-# (0, 'AG1200', 40, 15, [0, 0, 0, 6, 0, 0, 0, 105, 89, 8, 0, 0, 0, 0, 0])
 #
-# Most output seems to be a short item 
-# (24, 'PLL', 61, 5, [0, 0, 0, 0, 0])
-# followed by a long item (len=43)
-# (35, 'PLL', 61, 43, [0, 0, 0, 1, 0, 24, 4, 214, 62, 32, 176, 242, 62, 100, 59, 223, 62, 90, 100, 251, 62, 106, 188, 52, 63, 139, 108, 71, 63, 72, 225, 58, 63, 123, 20, 78, 63, 0, 0, 3, 0, 1, 0])
+# Copyright 2019 Steve White
 #
-# Could this be a routing connection? First is from, second is to?
+# Permission is hereby granted, free of charge, to any person obtaining 
+# a copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the 
+# Software is furnished to do so, subject to the following conditions:
 #
-# First 4 bytes of the data often look like tile coordinates, but 
-# not always...
-
-import os
+# The above copyright notice and this permission notice shall be included 
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+#
 import struct
 import sys
 
-small_types = [0, 3, 4]
-std_types = [44, 61, 53, 78, 66, 47, 77, 36, 41, 42, 43]
-last_type = None
-last_len = 0
-pos = 0
+if len(sys.argv) != 2:
+    print("usage: %s <uncompressed bar file>" % sys.argv[0])
+    sys.exit(-1)
+
+def skip(file, n):
+    file.read(n)
+
+def read16(file):
+    return struct.unpack('H', file.read(2))[0]
+
+def read32(file):
+    return struct.unpack('I', file.read(4))[0]
+
+def read_string(file):
+    len = read16(file)
+    return file.read(len)
+
 with open(sys.argv[1], "rb") as input_file:
-    size = os.fstat(input_file.fileno()).st_size
-    print(size)
-    while pos < size:
-        name_len = struct.unpack('h', input_file.read(2))[0]
-        name = input_file.read(name_len)
-        type = ord(input_file.read(1))
+    device = read_string(input_file)
+    print("Device: %s" % device)
+    skip(input_file, 4)
+    skip(input_file, 4)
+    entries = read32(input_file)
+    print("Number of entries: %s" % entries)
+
+    for i in range(0, entries):
+        entry = []
+    
+        # Seems like a tile coordinate.
+        # First value ranges from 0 to 12.
+        # Second ranges from 0 to 9. 
+        # AG1KLP is 10 x 14.  
+        # First value being one short could be 
+        # because the last column on the chip
+        # is all Rogic
+        entry.append(read16(input_file))
+        entry.append(read16(input_file))
+    
+        # Tile name. Seen values:
+        # BOOT_PLL, BRAM0, ION0_4_0, ION1_4_1, IOS0_4_0, IOS1_4_1, LOGIC0, PLL
+        entry.append(read_string(input_file))
+
+        # Seems like a type.. values are:
+        # 36, 41, 42, 43, 44, 47, 53, 61, 66, 77, 78
+        entry.append(read16(input_file))
+
+        # First value here ranges from 1 to 15, and then 65535
+        entry.append(read16(input_file))
+
+        # Second value ranges from 1 to 35
+        entry.append(read16(input_file))
+
+        # Tile name. Same values as above
+        entry.append(read_string(input_file))
+
+        # Type?  Same values as above.
+        entry.append(read16(input_file))
+
+        # Same 1 to 15, 65535
+        entry.append(read16(input_file))
+
+        # Ranges from 1 to 67
+        entry.append(read16(input_file))
+
+        # Seems like timing information
+        for _ in range(0, 8):
+            entry.append(read32(input_file))
+    
+        # number of paths??
+        repeat = read16(input_file)
+        entry.append(repeat)
+
+        for _ in range(0, repeat):
+            row = []
+            # Always seems to be 9 or 10
+            row.append(read16(input_file))
+
+            # Always seems to be 1
+            row.append(read16(input_file))
+
+            # Always seems to be LOGIC0 or BRAM0
+            row.append(read_string(input_file))
         
-        if type == 40:    # device header?
-            val_len = 15 
-        elif type in small_types:
-            val_len = 7
-        elif type in std_types:
-            if last_len == 5:
-                val_len = 43 
-            else:
-                val_len = 5 
-        else:
-            print("Unknown type! %s" % (type))
+            # Always seems to be 0, 3, 4, 36
+            row.append(read16(input_file))
+
+            # Ranges from 1 to 90
+            row.append(read16(input_file))
         
-        last_type = type
-        last_len = val_len
-        i = 0
-        val = []
-        while i < val_len:
-            try:
-                val.append(ord(input_file.read(1)))
-                i = i + 1
-                if type != 66 and val_len == 5 and i == val_len and val[-2] == 9:
-                    val_len = val_len + 2
-            except:
-                break
-                
-        print(pos, name, type, val_len, val)
-        pos = pos + 2 + name_len + 1 + val_len
+            entry.append(row)
+
+        print(entry)
