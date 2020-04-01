774bd1a0c9a83628bb8b480e3787c8f8159b5d26
Steve White <swhite@lekker.local>
2020-04-01 09:56:13 -0700

    invoke fasm_pack.py to turn the fasm into an ascii bitstream file and invoke agm-pack.py to convert the asc into a binary bitstream

--- nextpnr/simple.sh
+++ nextpnr/simple.sh
@@ -4,3 +4,5 @@ yosys -p "tcl synth/synth_generic.tcl 4 blinky.json" blinky.v
 # --pre-place simple_timing.py
 nextpnr-generic --pre-pack simple.py --post-route bitstream.py --json blinky.json --write pnrblinky.json --debug
 yosys -p "read_verilog -lib synth/prims.v; read_json pnrblinky.json; dump -o blinky.il; show -format png -prefix blinky"
+python3 fasm_pack.py blinky.fasm blinky.asc
+python3 ../bitstream/agm-pack.py blinky.asc blinky.bin
