d51e332fcfaa0b74c128f2fb80cb439b61678fcb
Steve White <swhite@lekker.local>
2020-04-01 15:48:41 -0700

    Update pins – two were invalid on AG1KLPQ48. Add comments to each indicating the pin number.

--- nextpnr/blinky.v
+++ nextpnr/blinky.v
@@ -1,25 +1,25 @@
 module top;
 
 	wire clk;
-	(* BEL="IOTILE(01,07):alta_rio00", keep *)
+	(* BEL="IOTILE(09,04):alta_rio00", keep *) /* PIN_46 */
 	GENERIC_IOB #(.INPUT_USED(1), .OUTPUT_USED(0)) clk_ibuf (.O(clk));
 
 	wire [7:0] leds;
-	(* BEL="IOTILE(01,02):alta_rio00", keep *)
+	(* BEL="IOTILE(01,02):alta_rio02", keep *)	/* PIN_11 */
 	GENERIC_IOB #(.INPUT_USED(0), .OUTPUT_USED(1)) led7_obuf (.I(leds[7]));
-	(* BEL="IOTILE(01,02):alta_rio01", keep *)
+	(* BEL="IOTILE(01,07):alta_rio00", keep *)	/* PIN_09 */
 	GENERIC_IOB #(.INPUT_USED(0), .OUTPUT_USED(1)) led6_obuf (.I(leds[6]));
-	(* BEL="IOTILE(01,04):alta_rio00", keep *)
+	(* BEL="IOTILE(09,06):alta_rio02", keep *)  /* PIN_06 */
 	GENERIC_IOB #(.INPUT_USED(0), .OUTPUT_USED(1)) led5_obuf (.I(leds[5]));
-	(* BEL="IOTILE(01,04):alta_rio01", keep *)
+	(* BEL="IOTILE(09,01):alta_rio01", keep *)  /* PIN_05 */
 	GENERIC_IOB #(.INPUT_USED(0), .OUTPUT_USED(1)) led4_obuf (.I(leds[4]));
-	(* BEL="IOTILE(01,05):alta_rio00", keep *)
+	(* BEL="IOTILE(09,00):alta_rio00", keep *)	/* PIN_04 */
 	GENERIC_IOB #(.INPUT_USED(0), .OUTPUT_USED(1)) led3_obuf (.I(leds[3]));
-	(* BEL="IOTILE(01,05):alta_rio01", keep *)
+	(* BEL="IOTILE(09,00):alta_rio02", keep *)	/* PIN_03 */
 	GENERIC_IOB #(.INPUT_USED(0), .OUTPUT_USED(1)) led2_obuf (.I(leds[2]));
-	(* BEL="IOTILE(01,05):alta_rio02", keep *)
+	(* BEL="IOTILE(09,01):alta_rio03", keep *)	/* PIN_02 */
 	GENERIC_IOB #(.INPUT_USED(0), .OUTPUT_USED(1)) led1_obuf (.I(leds[1]));
-	(* BEL="IOTILE(01,05):alta_rio03", keep *)
+	(* BEL="IOTILE(09,02):alta_rio00", keep *)	/* PIN_01 */
 	GENERIC_IOB #(.INPUT_USED(0), .OUTPUT_USED(1)) led0_obuf (.I(leds[0]));
 
 reg [25:0] ctr;
