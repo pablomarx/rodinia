53fe91e4ca80d08edf51cc2e662a50d67db276de
Steve White <swhite@lekker.local>
2020-04-07 15:09:42 -0700

    During encode, translate FF_USED into LUTCMUX

--- bitstream/tiles.py
+++ bitstream/tiles.py
@@ -1414,11 +1414,12 @@ InstallTile(Tile('ALTA_TILE_SRAM_DIST', 'LogicTILE', columns=34, rows=68, slices
 }, key_transformers={
     'IMUX[0-9][0-9]': lambda x: "alta_slice%02i_%s" % (int(int(x[4:]) / 4), x),
     'OMUX[0-9][0-9]': lambda x: "alta_slice%02i_%s" % (int(int(x[4:]) / 3), x),
-    'alta_slice[0-9][0-9].FF_USED': lambda x: True,
+    'alta_slice[0-9][0-9].FF_USED': lambda x: "alta_slice%02i_LUTCMUX" % (int(x[10:12])),
     'alta_slice[0-9][0-9].INIT\[[^\]]*]': lambda x: re.sub('(alta_slice[0-9][0-9]).INIT\[[^\]]*]', lambda x: x.groups()[0] + "_LUT", x),
 }, encoders={
     'alta_slice[0-9][0-9]_IMUX[0-9][0-9]': lambda key,val: mux_encode(bits_to_num(val), 9, 3),
     'alta_slice[0-9][0-9]_LUT$': lambda key,val: lut_encode(key,val),
+    'alta_slice[0-9][0-9]_LUTCMUX': lambda key,val: [val[0], 0],
     'RMUX[0-9][0-9]': lambda key,val: mux_encode(bits_to_num(val), 7, 3),
     'CtrlMUX[0-9][0-9]': lambda key,val: mux_encode(bits_to_num(val), 8, 4),
 	'TileClkMUX[0-9][0-9]': lambda key,val: mux_encode(bits_to_num(val), 4, 0),
