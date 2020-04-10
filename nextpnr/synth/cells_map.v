24d7ea8146030ca95bcc2071aed1d1f195c2d172
Steve White <swhite@lekker.local>
2020-04-09 20:08:22 -0700

    alter luts based on width.  results now look like the vqm yosys generates, flatten.vx and filtered.vx

--- nextpnr/synth/cells_map.v
+++ nextpnr/synth/cells_map.v
@@ -4,7 +4,17 @@ module \$lut (A, Y);
 	input [WIDTH-1:0] A;
 	output Y;
 
-	LUT #(.K(`LUT_K), .INIT(LUT)) _TECHMAP_REPLACE_ (.I(A), .Q(Y));
+   generate
+	if (WIDTH == 2) begin
+	LUT #(.K(`LUT_K), .INIT({4{LUT}})) _TECHMAP_REPLACE_ (.I({1'b1, 1'b1, A[1], A[0]}), .Q(Y));
+	end else
+		if(WIDTH == 3) begin
+			LUT #(.K(`LUT_K), .INIT({2{LUT}})) _TECHMAP_REPLACE_ (.I({1'b1, A[2], A[1], A[0]}), .Q(Y));
+		end else
+			if(WIDTH == 4) begin
+				LUT #(.K(`LUT_K), .INIT(LUT)) _TECHMAP_REPLACE_ (.I(A), .Q(Y));
+			end 
+    endgenerate
 endmodule
 
 module  \$_DFF_P_ (input D, C, output Q); DFF  _TECHMAP_REPLACE_ (.D(D), .Q(Q), .CLK(C)); endmodule
